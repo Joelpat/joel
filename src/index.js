137ac95791ba93e0ef6afeda7e178e1fe9064c16
JojoSala <joelsalacode23gmail.com>
2024-04-06 10:39:03 +0200

    new commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Routes, Route } from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Welcome from './pages/welcome';
 import Home from './pages/home';
 import Biofloc from './pages/biofloc';
